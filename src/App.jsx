1a7b774c12b999b7f8b8f0340dd26614f3636dc2
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-02-13 18:39:41 -0300

    feat: WorldWise App - CSS Modules - React Route - Nested routes

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Product from "./pages/Product"
+import Pricing from "./pages/Pricing"
+import Homepage from "./pages/Homepage"
+import Login from "./pages/Login"
+import AppLayout from "./pages/AppLayout"
+import PageNotFound from "./pages/PageNotFound"
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="login" element={<Login />} />
+                <Route path="app" element={<AppLayout />}>
+                    <Route path="cities" element={<p>List of cities</p>} />
+                    <Route path="countries" element={<p>List of countries</p>} />
+                    <Route path="form" element={<p>Form</p>} />
+                </Route>
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App
---
+++ src/components/AppNav.jsx
@@ -0,0 +1,11 @@
+import styles from "./AppNav.module.css";
+
+function AppNav() {
+    return (
+        <div className={styles.nav}>
+            AppNav
+        </div>
+    )
+}
+
+export default AppNav
